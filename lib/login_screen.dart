52e366992e87fe3052d0a53e641611a8380acfed
unknown <0214gxqs@gmail.com>
2025-01-20 23:34:19 +0900

    Update 'log in' wording to 'sign in'

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // Firebase 인증 패키지
+import 'phone_auth_screen.dart'; // 전화번호 인증 화면을 위한 파일 추가
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -113,12 +114,24 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _login,
-                child: const Text('Log In'),
+                child: const Text('Sign in'),
               ),
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: _signUp,
-                child: const Text('Sign Up'),
+                child: const Text('Sign up'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // 전화번호 인증 화면으로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PhoneAuthScreen()),
+                  );
+                },
+                child: const Text('Sign in with Phone Number'),
               ),
             ],
           ),
