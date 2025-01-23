204a46587d8c30f0a30b7441646a8e979b3e2ad1
unknown <0214gxqs@gmail.com>
2025-01-23 23:50:06 +0900

    Change the title of the login page from 'Login' to 'Log in'

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -80,7 +80,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Sign in'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
