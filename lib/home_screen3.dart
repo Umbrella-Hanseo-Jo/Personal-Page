c80d1b1adc1e9f5176d8c1ea30df1222d7ed0bc1
unknown <0214gxqs@gmail.com>
2025-02-01 23:56:07 +0900

    Change behavior to keep music playing when back button is pressed(5)

--- lib/home_screen3.dart
+++ lib/home_screen3.dart
@@ -1,62 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart'; // audioplayers 패키지
 import 'package:flutter/foundation.dart'; // kIsWeb을 사용하기 위한 패키지
-import 'package:back_button_interceptor/back_button_interceptor.dart'; // back_button_interceptor 패키지
+import 'package:provider/provider.dart'; // Provider 패키지
+import 'audio_provider.dart'; // AudioProvider 설정 파일
 
-class HomeScreen3 extends StatefulWidget {
+class HomeScreen3 extends StatelessWidget {
   const HomeScreen3({super.key});
 
   @override
-  State<HomeScreen3> createState() => _HomeScreen3State();
-}
-
-class _HomeScreen3State extends State<HomeScreen3> {
-  final AudioPlayer _audioPlayer = AudioPlayer(); // AudioPlayer 객체 생성
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 뒤로 가기 버튼 동작을 가로채는 인터셉터
-    BackButtonInterceptor.add((stopDefaultButtonEvent, routeInfo) =>
-        _onBackPressed(stopDefaultButtonEvent));
-    // 웹에서는 사용자의 상호작용을 기다려야 하므로 자동으로 음악을 재생하지 않음
-    if (!kIsWeb) {
-      // 모바일에서는 앱 실행 시 음악 자동 재생
-      _playBackgroundMusic();
-    }
-  }
-
-  // 배경 음악 재생 메소드
-  void _playBackgroundMusic() async {
-    // assets 폴더 내의 오디오 파일 재생
-    await _audioPlayer.setSource(AssetSource('happy.mp3'));
-    await _audioPlayer.setReleaseMode(ReleaseMode.loop); // 반복 재생
-    await _audioPlayer.resume(); // 음악 재생 시작
-  }
-
-  // 배경 음악 정지하는 메소드
-  void _stopBackgroundMusic() async {
-    await _audioPlayer.stop(); // 음악 정지
-  }
-
-  // 뒤로가기 버튼을 눌렀을 때의 동작을 정의하는 함수
-  bool _onBackPressed(bool stopDefaultButtonEvent) {
-    Navigator.pop(context); // 페이지 닫기, 음악 계속 재생
-    return true; // 뒤로가기 동작 가로챔
-  }
-
-  @override
-  void dispose() {
-    // 뒤로가기 인터셉터 제거
-    BackButtonInterceptor.remove((stopDefaultButtonEvent, routeInfo) =>
-        _onBackPressed(stopDefaultButtonEvent));
-
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    // AudioProvider를 가져옵니다.
+    final audioProvider = Provider.of<AudioProvider>(context);
+
+    // 웹에서는 사용자의 상호작용을 기다려야 하므로 자동으로 음악을 재생하지 않음
+    if (!kIsWeb && !audioProvider.isPlaying) {
+      // 모바일에서는 앱 실행 시 음악 자동 재생
+      audioProvider.play('happy.mp3');
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('HAPPY'),
@@ -88,12 +48,16 @@ class _HomeScreen3State extends State<HomeScreen3> {
                 const SizedBox(height: 20), // 간격 추가
 
                 ElevatedButton(
-                  onPressed: _playBackgroundMusic, // 음악 재생
+                  onPressed: () {
+                    audioProvider.play('happy.mp3'); // 음악 재생
+                  },
                   child: const Text('음악 시작'),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: _stopBackgroundMusic, // 음악 정지
+                  onPressed: () {
+                    audioProvider.stop(); // 음악 정지
+                  },
                   child: const Text('음악 정지'),
                 ),
                 const SizedBox(height: 20),
